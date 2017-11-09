cea79f1ec6f45698bd1dfe4fe497d03429c4450f
Ana Asins <asinsanna@gmail.com>
2017-11-09 09:20:42 +0100

    js de fotos

---
+++ js/fotos.js
@@ -0,0 +1,11 @@
+function fotosGrandes(elemento) {
+  var foto= document.getElementById(elemento.id);
+  foto.style.width='300px';
+  foto.style.height='auto';
+
+}
+function fotosPequenas(elemento) {
+  var foto= document.getElementById(elemento.id);
+  foto.style.width='220px';
+  foto.style.height='auto';
+}
