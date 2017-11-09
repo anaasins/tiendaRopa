4e98fa957c208eb1bdf1b217895f6d2a3aaaba95
Borja Molina <borjacofrentino@gmail.com>
2017-11-09 09:46:39 +0100

    Funcion para validar el formulario de contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,31 @@
 
+//Funcion para validar si lo que escribimos en los fomularios es lo que tiene que ser
+function validarTexto(){
+          /*Variables que guardan los valores de los campos*/
+          nombre1 = document.getElementById("resultadonom").value;
+          telefono1 = document.getElementById("resultadotlf").value;
+          //Primer if para comprobar que el nombre solo hay letras
+          if (!/^([a-zA-Z])*$/.test(nombre1)){
+            nombre1 = document.getElementById("resultadonom");
+            color=nombre1.style.background = "red";
+            var return1="return false";
+          }
+          //Segundo if para comprobar que el telefono solo hay numeros
+          if (!/^([0-9])*$/.test(telefono1)){
+            telefono1 = document.getElementById("resultadotlf");
+            color=telefono1.style.background = "red";
+            var return2="return false";
+      }else {
+        return3="return true";
+      }
+      //If final para que si en alguno de los dos if son falsos no pueda enviar
+      if (return1=="return false" || return2=="return false") {
+        return false;
+      }else if (return3=="return true") {
+        return true;
+      }
+        }
+
 function imprimeMapa() {
   //creamos el div donde ira el mapa y le damos el css deseado.
   var mapa= document.createElement('div');
