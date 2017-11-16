f4777b6ea76fb18eb50079101856b981204dec04
Borja Molina <borjacofrentino@gmail.com>
2017-11-16 10:42:25 +0100

    Termino todo lo de carrito y añado una cosa al css

--- js/carrito.js
+++ js/carrito.js
@@ -32,3 +32,49 @@ function imprimeCarrito() {
     foto.innerHTML="<img class='fotoh' id='"+arrayProductos[i]+"' src='img/"+arrayProductos[i]+".jpg'></img>"
   }
 }
+
+// Funcion de contador que borra todo cuando termina la cuenta atras
+var toHour=0;
+var toMinute=10;
+var toSecond=0;
+function cuentaAtras()
+{
+
+	toSecond=toSecond-1;
+	if(toSecond<0)
+	{
+		toSecond=59;
+		toMinute=toMinute-1;
+	}
+	form.second.value=toSecond;
+
+	if(toMinute<0)
+	{
+		toMinute=59;
+		toHour=toHour-1;
+	}
+	form.minute.value=toMinute;
+
+	form.hour.value=toHour;
+	if(toHour<0)
+	{
+		//final
+		form.second.value=0;
+		form.minute.value=0;
+		form.hour.value=0;
+	}else{
+		setTimeout("cuentaAtras()",1000);
+	}
+  //If para cundo se termine el tiempo
+  if (toSecond==0 && toMinute==0 && toHour==0) {
+    localStorage.clear();
+    window.location='carrito.html';
+  }
+
+}
+
+//Boton para borrar el localStorage
+function borrar(){
+  localStorage.clear();
+  window.location='carrito.html';
+}
